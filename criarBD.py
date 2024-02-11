0570c2d199dfb87f9d54f8a42c5fec93efd5134c
igor keven <102704465+igorkeven@users.noreply.github.com>
2024-02-11 01:16:21 -0300

    Add files via upload

---
+++ criarBD.py
@@ -0,0 +1,46 @@
+import sqlite3
+
+# Conecte-se ao banco de dados SQLite (será criado se não existir)
+conn = sqlite3.connect('usuarios.db')
+
+# Crie um cursor para executar comandos SQL
+cursor = conn.cursor()
+
+# Crie a tabela usuario
+cursor.execute("""
+CREATE TABLE usuario (
+    id INTEGER PRIMARY KEY,
+    nome TEXT NOT NULL,
+    senha TEXT NOT NULL,
+    tema TEXT DEFAULT '#3b5998',
+    img_perfil TEXT DEFAULT '/static/imagens/user.png',
+    img_capa TEXT DEFAULT '/static/imagens/fundo.jpg'
+)
+""")
+
+
+
+# Crie a tabela amigos
+cursor.execute("""
+CREATE TABLE amigos (
+    usuario_id INTEGER,
+    amigo_id INTEGER,
+    PRIMARY KEY (usuario_id, amigo_id),
+    FOREIGN KEY(usuario_id) REFERENCES usuario(id),
+    FOREIGN KEY(amigo_id) REFERENCES usuario(id)
+)
+""")
+
+# Crie a tabela solicitacoes_amizade
+cursor.execute("""
+CREATE TABLE solicitacoes_amizade (
+    solicitante_id INTEGER,
+    solicitado_id INTEGER,
+    PRIMARY KEY (solicitante_id, solicitado_id),
+    FOREIGN KEY(solicitante_id) REFERENCES usuario(id),
+    FOREIGN KEY(solicitado_id) REFERENCES usuario(id)
+)
+""")
+
+# Feche a conexão com o banco de dados
+conn.close()
